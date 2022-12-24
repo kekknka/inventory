4c97641fba2444b120dc36e5d54dd581d5cc41d5
kekknka <jairogiovanydelacruzmarcial@hotmail.com>
2022-12-24 11:49:38 -0600

    Update migrations

---
+++ database/migrations/2022_12_24_155409_create_sells_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sells', function (Blueprint $table) {
+            $table->id();
+            $table->integer('operation_type');
+            $table->double('subtotal', 10 , 2);
+            $table->double('discount', 10, 2);
+            $table->double('total', 10, 2);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sells');
+    }
+};
